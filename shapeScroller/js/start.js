7ca1bbdde78472bbb291f73090059c8d3a28bd3c
vincewong22 <vincewong22@gmail.com>
2016-05-04 18:29:22 -0700

    reorganize start methods

--- shapeScroller/js/start.js
+++ shapeScroller/js/start.js
@@ -1,19 +1,78 @@
-
+var id = [];
+var level_shapes = [[], [], []], user_shapes = [[], [], []];
 
 $(document).ready(function () {
-
 //Set all elements with init id with block class drag enabled
-    var id = [];
-    var level_shapes = [[], [], []], user_shapes = [[], [], []];
+    init();
+//button that checks the
+    $( "#click" ).click(function() {
+        getListContent();
+    });
+//button that checks the
+    $("#start_click").click(function () {
+        level_shapes = getListContent();
+        localStorage.setItem("level_shapes",level_shapes.toString());
+        window.location.href = "input.html";
+    });
+    $("#input_click").click(function () {
+        user_shapes = getListContent();
+        //alert(document.cookie);
 
+        var level_shapes_str = localStorage.getItem("level_shapes");
+        if((user_shapes.toString()) == level_shapes_str)
+            alert("match");
+        else
+            alert(level_shapes_str+" no match!");
+    });
+
+//gets the shapes from the 3 lists
+    function getListContent() {
+        var shapelist0 = [], shapelist1 = [], shapelist2 = [];
+        var shapeArray = [shapelist0, shapelist1, shapelist2];
+        for (i = 0; i < shapeArray.length; i++) {
+            var target = "#shapelist" + i + " div";
+            $(target).each(function () {
+                if ($(this).hasClass("colorRed"))
+                    myClass = "_red"
+                if ($(this).hasClass("colorGreen"))
+                    myClass = "_green"
+                if ($(this).hasClass("colorBlue"))
+                    myClass = "_blue"
+                shapeArray[i].push(this.id + myClass);
+
+            });
+
+        }
+        return shapeArray;
+    }
+    startTimer();
+});//end of ready
+
+function startTimer(){
+    var countdown =  $("#countdown").countdown360({
+        radius      : 60,
+        seconds     : 10,
+        fontColor   : '#FFFFFF',
+        autostart   : false,
+        onComplete : function() {window.location = "input.html"}
+        <!--onComplete  : function() { console.log('done') } -->
+    });
+    countdown.start();
+    console.log('countdown360 ',countdown);
+}
+
+function init(){
     $("#init .block").draggable({ helper: "clone",
         connectToSortable: ".list",
         drop: function (event, ui) {
 
         }
     });
+    sortList();
+}
 
-//Connect empty sorted lists with draggable elements
+function sortList(){
+    //Connect empty sorted lists with draggable elements
     var removeIntent = false;
     $(".list").sortable({
 
@@ -36,56 +95,7 @@ $(document).ready(function () {
         },
         beforeStop: function (event, ui) {
             console.log(removeIntent);
-                newItem = ui.item;
+            newItem = ui.item;
         }
     });
-//button that checks the
-    $( "#click" ).click(function() {
-        getListContent();
-    });
-//button that checks the
-    $("#start_click").click(function () {
-        level_shapes = getListContent();
-        localStorage.setItem("level_shapes",level_shapes.toString());
-        window.location.href = "input.html";
-    });
-    $("#input_click").click(function () {
-        user_shapes = getListContent();
-        //alert(document.cookie);
-
-        var level_shapes_str = localStorage.getItem("level_shapes");
-        if((user_shapes.toString()) == level_shapes_str)
-            alert("match");
-        else
-            alert(level_shapes_str+" no match!");
-    });
-
-
-
-//gets the shapes from the 3 lists
-    function getListContent() {
-        var shapelist0 = [], shapelist1 = [], shapelist2 = [];
-        var shapeArray = [shapelist0, shapelist1, shapelist2];
-        for (i = 0; i < shapeArray.length; i++) {
-            var target = "#shapelist" + i + " div";
-            $(target).each(function () {
-                if ($(this).hasClass("colorRed"))
-                    myClass = "_red"
-                if ($(this).hasClass("colorGreen"))
-                    myClass = "_green"
-                if ($(this).hasClass("colorBlue"))
-                    myClass = "_blue"
-                shapeArray[i].push(this.id + myClass);
-
-            });
-
-        }
-
-//debug strings
-//        $('#output0').html("1:"+shapeArray.toString());
-//        $('#output1').html("2:"+shapelist1.toString());
-//        $('#output2').html("3:"+shapelist2.toString());
-        return shapeArray;
-    }
-    startTimer();
-});//end of ready
+}
